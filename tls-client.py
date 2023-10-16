96a2a3c76faf3238c6ec7ee178b286d04b11903e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2023-10-16 23:51:37 +0000

    Try TLS client socket with default context.
    Print negotiated cipher for debugging.

--- tls-client.py
+++ tls-client.py
@@ -7,3 +7,4 @@ context = ssl.create_default_context()
 with socket.create_connection((HOSTNAME, 443)) as sock:
     with context.wrap_socket(sock, server_hostname=HOSTNAME) as ssock:
         print(ssock.version())
+        print(ssock.cipher())
