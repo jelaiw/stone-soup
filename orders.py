00128ae16c8f7e2dbb3a7dae65f76af10f0db08d
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-04-27 00:37:57 +0000

    Vulnerable code from Kohnfelder (2021).

---
+++ orders.py
@@ -0,0 +1,19 @@
+from collections import namedtuple
+PurchaseOrder = namedtuple('PurchaseOrder', 'id, date, items')
+LineItem = namedtuple('LineItem', 'kind, detail, amount, quantity', defaults=(1,))
+
+def validorder(po):
+    products = []
+    net = 0
+    for item in po.items:
+        if item.kind == 'payment':
+            net += item.amount
+        elif item.kind == 'product':
+            products.append(item)
+            net -= item.amount * item.quantity
+        else:
+            return "Invalid LineItem type: %s" % item.kind
+    
+    if net != 0:
+        return "Payment imbalance: $%0.2f." % net
+    return products
