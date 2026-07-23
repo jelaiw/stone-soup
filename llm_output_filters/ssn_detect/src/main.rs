f8f17bc5235beb2e465618f013610e1cc21b1105
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-07-23 00:01:02 +0000

    Replace re.find with is_match.
    Per closer read of API docs at https://docs.rs/regex/latest/regex/struct.Regex.html#method.find.

--- llm_output_filters/ssn_detect/src/main.rs
+++ llm_output_filters/ssn_detect/src/main.rs
@@ -5,15 +5,13 @@ fn contains_ssn(input_string: &str) -> bool {
     // https://docs.rs/regex/latest/regex/struct.Regex.html#method.new
     let re = Regex::new(ssn_pattern).unwrap();
 
-    match re.find(input_string) {
-        Some(_) => true,
-        None => false,
-    }
+    // https://docs.rs/regex/latest/regex/struct.Regex.html#method.is_match
+    return re.is_match(input_string);
 }
 
 fn main() {
-//    let test_input = "My Social Security number is 123-45-6789.";
-    let test_input = "No number here!";
+    let test_input = "My Social Security number is 123-45-6789.";
+//    let test_input = "No number here!";
 
     if contains_ssn(test_input) {
         println!("Detected SSN.");
